2db1451c18929aaf0a8a1f92a427c75dd4a54f86
eranda-ihalagedara <67923594+eranda-ihalagedara@users.noreply.github.com>
2023-08-09 14:16:25 +1000

    Update comments plot2.R

--- 4.Exploratory Data Analysis/Course Project 1/plot2.R
+++ 4.Exploratory Data Analysis/Course Project 1/plot2.R
@@ -9,7 +9,7 @@ library(lubridate)
 power_data = read_delim("household_power_consumption.txt", delim=";", col_names = TRUE, na="?") %>% 
   clean_names()
 
-# Convert date column to date type and filter the dates 2007-02-01 and 2007-02-02
+# Create date_time column and filter the dates 2007-02-01 and 2007-02-02
 power_data = power_data %>% 
   mutate(date_time = as.POSIXlt(paste(date,time), format = "%d/%m/%Y %H:%M")) %>% 
   filter(date_time >= "2007-02-01" & date_time < "2007-02-03")
